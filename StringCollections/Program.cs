ce80948e8d6e5b8d878df8002190634523b36b43
Manuel Geraldes <manuelgeraldes2009@hotmail.com>
2022-05-12 14:03:48 +0100

    Add all strings from array to each collection

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -11,6 +11,19 @@ namespace StringCollections
             Stack<string> testStack = new Stack<string>();
             Queue<string> testQueue = new Queue<string>();
             HashSet<string> testHash = new HashSet<string>();
+
+            string[] testStrings = new string[] 
+            {
+                "ola", "mundo", "adeus", "mundo", "!"
+            };
+
+            for (int i = 0; i < testStrings.Length; i++)
+            {
+                testList.Add(testStrings[i]);
+                testStack.Push(testStrings[i]);
+                testQueue.Enqueue(testStrings[i]);
+                testHash.Add(testStrings[i]);
+            }
         }
     }
 }
